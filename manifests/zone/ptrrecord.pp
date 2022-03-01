8e6c154332285717a99505b1817a840b1c5eb9e1
mherrre <74656723+mherrre@users.noreply.github.com>
2022-03-01 18:01:37 +0100

    Update ptrrecord.pp

--- manifests/zone/ptrrecord.pp
+++ manifests/zone/ptrrecord.pp
@@ -1,4 +1,17 @@
-<%- if defined?(@description) -%>
-; <%= @description %>
-<%- end -%>
-<%= @record %><% if @append_zonename %>.<%= @zonename %>.<% end %>          <% if defined?(@ttl) %><%= @ttl %> <% end %><%= @class %> PTR <%= @value %>
+define named::zone::ptrrecord (
+                                $value,
+                                $zonename,
+                                $record          = $name,
+                                $ttl             = undef,
+                                $class           = 'IN',
+                                $order           = '42',
+                                $description     = undef,
+                                $append_zonename = true,
+                              ) {
+
+  concat::fragment{ "CNAME ${record}/${value} record ${named::params::zonedir}/${zonename}":
+    target  => "${named::params::zonedir}/${zonename}",
+    content => template("${module_name}/zone/ptrrecord.erb"),
+    order   => "99-${order}",
+  }
+}
