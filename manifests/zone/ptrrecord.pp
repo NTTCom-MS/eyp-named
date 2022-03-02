e9d6ae7bd6fff01fa2bd1728fdd702c93baff827
mherrre <74656723+mherrre@users.noreply.github.com>
2022-03-02 10:56:13 +0100

    Update ptrrecord.pp

--- manifests/zone/ptrrecord.pp
+++ manifests/zone/ptrrecord.pp
@@ -9,7 +9,7 @@ define named::zone::ptrrecord (
                                 $append_zonename = true,
                               ) {
 
-  concat::fragment{ "CNAME ${record}/${value} record ${named::params::zonedir}/${zonename}":
+  concat::fragment{ "PTR ${record}/${value} record ${named::params::zonedir}/${zonename}":
     target  => "${named::params::zonedir}/${zonename}",
     content => template("${module_name}/zone/ptrrecord.erb"),
     order   => "99-${order}",
